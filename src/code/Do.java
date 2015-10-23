ed3c7eb9ce273c44ac79344750be03fbc2f76cef
William-Hunter <a709209619@qq.com>
2015-10-23 12:20:49 +0800

    I use function to shift stack.

--- src/code/Do.java
+++ src/code/Do.java
@@ -8,17 +8,24 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Do {
-	void list(String NewsURL) throws IOException{		
-		Document doc=Jsoup.connect(NewsURL).get();
+	void page(String PaperURL) throws IOException{	//这个方法和下面的list()取代了原本的list()，这是循环调用的方式，不会有大量的stack，性能更稳定
+		String nextLink=list(PaperURL);
+		while(nextLink!=null){
+			nextLink=list(nextLink);
+		}
+	}
+	String list(String PaperURL) throws IOException{		
+		Document doc=Jsoup.connect(PaperURL).get();
 		Elements es_NSlink=doc.select("a.c67738");
 		for(Element e_NSlink:es_NSlink){
 			atricle(e_NSlink.absUrl("href"));
 		}
 		Element NextPage=doc.select("a.Next").first();
 		if(NextPage!=null){
-			list(NextPage.artiUrl("href"));
-		}
-		
+			return NextPage.absUrl("href");
+		}else{
+			return null;
+		}	
 	}
 	void atricle(String artiUrl) throws IOException {
 		Document doc=Jsoup.connect(artiUrl).get();
@@ -29,4 +36,24 @@ public class Do {
 		Element text=doc.select("div#vsb_newscontent").first();
 		System.out.println(text.text()+"\n");
 	}
+
+/*//这是使用递归调用的方法，会产生大量的stack，
+ * 
+ * void list(String NewsURL) throws IOException{		
+		Document doc=Jsoup.connect(NewsURL).get();
+		Elements es_NSlink=doc.select("a.c67738");
+		for(Element e_NSlink:es_NSlink){
+			atricle(e_NSlink.absUrl("href"));
+		}
+		Element NextPage=doc.select("a.Next").first();
+		if(NextPage!=null){
+			list(NextPage.absUrl("href"));
+		}	
+	}
+ * 
+ * 
+ * */
+
+
+
 }
--- src/code/Main.java
+++ src/code/Main.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Do obj=new Do();
-		obj.Page("http://www.zhzhu.edu.cn/index/xxxw.htm");
-		obj.Page("http://www.zhzhu.edu.cn/index/tzgg.htm");
+		obj.page("http://www.zhzhu.edu.cn/index/xxxw.htm");
+		obj.page("http://www.zhzhu.edu.cn/index/tzgg.htm");
 		System.out.println("spider is finish***********");
 	}
 }
