4918c28b43b88e5b987ad4d5b017a01dd708e202
William-Hunter <a709209619@qq.com>
2015-10-30 10:30:31 +0800

    add logger info

--- src/code/Do.java
+++ src/code/Do.java
@@ -8,15 +8,16 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Do {
-//	private slf4j logger=new slf4j();
-	void page(String PaperURL) throws IOException{	//这个方法和下面的list()取代了原本的list()，这是循环调用的方式，不会有大量的stack，性能更稳定
-		String nextLink=list(PaperURL);
+	int count=0;
+//	Logger logger = LoggerFactory.getLogger(Do.class);   
+	void page(String PageURL) throws IOException{	//这个方法和下面的list()取代了原本的list()，这是循环调用的方式，不会有大量的stack，性能更稳定
+		String nextLink=list(PageURL);
 		while(nextLink!=null){
 			nextLink=list(nextLink);
 		}
 	}
-	String list(String PaperURL) throws IOException{		
-		Document doc=Jsoup.connect(PaperURL).get();
+	String list(String PageURL) throws IOException{		
+		Document doc=Jsoup.connect(PageURL).get();
 		Elements es_NSlink=doc.select("a.c67738");
 		for(Element e_NSlink:es_NSlink){
 			atricle(e_NSlink.absUrl("href"));
@@ -30,31 +31,33 @@ public class Do {
 	}
 	void atricle(String artiUrl) throws IOException {
 		Document doc=Jsoup.connect(artiUrl).get();
+		count++;
+		System.out.println("\n"+count+"URL---->>"+artiUrl);
 		Element title=doc.select("td.titlestyle67448").first();
 		System.out.println("《《"+title.text()+"》》");
 		Element time=doc.select("span.timestyle67448").first();
 		System.out.println(time.text());
 		Element text=doc.select("div#vsb_newscontent").first();
-		System.out.println(text.text()+"\n");
-	}
-
-/*//这是使用递归调用的方法，会产生大量的stack，
- * 
- * void list(String NewsURL) throws IOException{		
-		Document doc=Jsoup.connect(NewsURL).get();
-		Elements es_NSlink=doc.select("a.c67738");
-		for(Element e_NSlink:es_NSlink){
-			atricle(e_NSlink.absUrl("href"));
+		System.out.println(text.text());
+		Elements images=doc.select("img[src]");
+		for(Element e:images){
+			System.out.println("\t"+e.attr("src"));
 		}
-		Element NextPage=doc.select("a.Next").first();
-		if(NextPage!=null){
-			list(NextPage.absUrl("href"));
-		}	
+		
 	}
- * 
- * 
- * */
 
-
+//这是使用递归调用的方法，会产生大量的stack，
+//  void list(String pageURL) throws IOException{		
+//		Document doc=Jsoup.connect(pageURL).get();
+//		Elements es_NSlink=doc.select("a.c67738");
+//		for(Element e_NSlink:es_NSlink){
+//			atricle(e_NSlink.absUrl("href"));
+//		}
+//		Element NextPage=doc.select("a.Next").first();
+//		if(NextPage!=null){
+//			list(NextPage.absUrl("href"));
+//		}	
+//	}
+ 
 
 }
--- src/code/Main.java
+++ src/code/Main.java
@@ -6,7 +6,10 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Do obj=new Do();
 		obj.page("http://www.zhzhu.edu.cn/index/xxxw.htm");
-		obj.page("http://www.zhzhu.edu.cn/index/tzgg.htm");
+//		obj.page("http://www.zhzhu.edu.cn/index/tzgg.htm");
+//		obj.atricle("http://www.zhzhu.edu.cn/info/1036/16577.htm");
 		System.out.println("spider is finish***********");
+		
+
 	}
 }
