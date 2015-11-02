ae5c5cc3ed052cbae17f8c381cc0e696bd710547
I like build <a709209619@qq.com>
2015-11-02 18:55:09 +0800

    add some comment

--- src/code/Main.java
+++ src/code/Main.java
@@ -4,15 +4,14 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		Do obj=new Do();
 		Logger logger = LoggerFactory.getLogger(Main.class);
-		logger.debug("spider is start");
+		logger.debug("spider is start**********");
 		obj.page("http://www.zhzhu.edu.cn/index/xxxw.htm");
 //		obj.page("http://www.zhzhu.edu.cn/index/tzgg.htm");
-//		obj.atricle("http://www.zhzhu.edu.cn/info/1036/13582.htm");
+//		obj.atricle("http://www.zhzhu.edu.cn/info/1036/13582.htm");//测试正文的
 		logger.debug("spider is finish***********");
 	}
 }
